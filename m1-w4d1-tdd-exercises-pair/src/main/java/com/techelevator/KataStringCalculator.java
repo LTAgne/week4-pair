e2fcfe48db215399c75c3fcf85c3dc7f58c1f3e0
Jared Malvic <jarediscool92@gmail.com>
2017-09-18 14:31:32 -0400

    Completed KataStringCalculator

--- m1-w4d1-tdd-exercises-pair/src/main/java/com/techelevator/KataStringCalculator.java
+++ m1-w4d1-tdd-exercises-pair/src/main/java/com/techelevator/KataStringCalculator.java
@@ -2,4 +2,25 @@ package com.techelevator;
 
 public class KataStringCalculator {
 
+	
+	public int convertStringToInt (String numbers) {
+		
+		int converted = 0;
+		
+		if (numbers.length() == 0) {}
+		
+		if (numbers.length() == 1) {
+			return converted += Integer.parseInt(numbers);
+		}
+		
+		if(numbers.length() >= 1) {
+			String splitter = numbers.substring(1, 2);
+			
+			String[] allNumbers = (numbers.split(splitter));
+			
+			for ( String thing : allNumbers) {				
+				converted += Integer.parseInt(thing);
+			}		
+		} return converted;
+	}
 }
--- m1-w4d1-tdd-exercises-pair/src/test/java/com/techelevator/KataStringCalculatorTest.java
+++ m1-w4d1-tdd-exercises-pair/src/test/java/com/techelevator/KataStringCalculatorTest.java
@@ -4,5 +4,50 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class KataStringCalculatorTest {
-
+	
+	KataStringCalculator sut = new KataStringCalculator() ;
+	
+	@Test 
+	public void test0() {		
+		assertEquals(0 , sut.convertStringToInt(""));		
+	}
+	
+	@Test
+	public void test1() {	
+		assertEquals(1, sut.convertStringToInt("1"));
+	}
+	
+	@Test
+	public void test2numberString() {		
+		assertEquals(3, sut.convertStringToInt("1,2"));
+	}
+	
+	@Test
+	public void test3numberString() {		
+		assertEquals(6, sut.convertStringToInt("1,2,3"));
+	}
+	
+	@Test
+	public void testWholeBunchaNumbers() {		
+		assertEquals(45, sut.convertStringToInt("1,2,3,4,5,6,7,8,9"));
+	}
+	
+	@Test
+	public void testSplitterVariable() {		
+		assertEquals(3, sut.convertStringToInt("1,2"));
+	}
+	
+	@Test
+	public void testSplitterVariableOther() {		
+		assertEquals(3, sut.convertStringToInt("1!2"));
+	}
+	
+	@Test
+	public void testSplitterVariableWithLineBreaks() {		
+		assertEquals(3, sut.convertStringToInt("1\n2"));
+	}
+	@Test
+	public void testSplitterVariableBonus() {		
+		assertEquals(3, sut.convertStringToInt("1;2"));
+	}
 }
